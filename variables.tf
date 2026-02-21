9f4d4c23cdc7c40f75a5442a81c1655b38085c8e
MNohava1987 <mnohava1987@gmail.com>
2026-02-20 21:44:08 -0600

    Fix: Replace invalid semicolons with idiomatic HCL in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
-variable "spacelift_api_key_id" { type = string; sensitive = true }
-variable "spacelift_api_key_secret" { type = string; sensitive = true }
+variable "spacelift_api_key_id" {
+  type      = string
+  sensitive = true
+}
 
-# Example: attach to these parent spaces so children inherit
-variable "baseline_space_ids" { type = list(string) default = ["gcp","azure"] }
+variable "spacelift_api_key_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "baseline_space_ids" {
+  type    = list(string)
+  default = []
+}
